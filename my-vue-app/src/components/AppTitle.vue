5015f46bea2de7848d273d8e197915b0ebd318d6
federicoalagna <federicoalagna01@gmail.com>
2024-05-29 19:29:11 +0200

    hello world

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppTitle'
+  }
+  </script>
+  
+  <style>
+  h1 {
+    color: #333;
+    font-size: 24px;
+    font-weight: bold;
+  }
+  </style>
